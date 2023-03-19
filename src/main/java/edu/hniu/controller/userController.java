8d39c6df82300ec85103c2c7c673909ca8c6f165
long <l19958126849@163.com>
2023-03-19 16:28:30 +0800

    first commit

---
+++ src/main/java/edu/hniu/controller/userController.java
@@ -0,0 +1,13 @@
+package edu.hniu.controller;
+
+/**
+ * @author long
+ * @create 2023-03-19 16:24
+ */
+
+public class userController {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
